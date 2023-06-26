37097f7132a1b4be2aaaff1fd0675a41775b1519
Gilbert <wghong@bu.edu>
2023-06-25 22:29:30 -0400

    edited out comments

--- recommendation.py
+++ recommendation.py
@@ -132,32 +132,24 @@ print("NaN count in days_since_prior_order column: ", orders[orders.days_since_p
 print("order_number 1 count in orders table: ", orders[orders.order_number == 1].drop_duplicates().shape[0])
 print("user_id count in orders table: ", orders.user_id.drop_duplicates().shape[0])
 
-"""由此可见, 在orders.csv里，缺失days_since_prior_order的行数，等同于order_number == 1的行数，也对应user的个数。
-
 2.2.Validate Valid orders matching in the prior table
-"""
 
-# orders.csv里面，不同eval_set的orders个数：
 orders.groupby(['eval_set'], as_index=False).agg(OrderedDict([('order_id','nunique')]))
-
-# 验证：order_products_prior.csv 里面的 order_id 都在 orders.csv 里 (确保所有prior orders仍然真实存在)
 print("order_id count in prior: ", order_products_prior['order_id'].nunique())
 print("order_id from prior found in orders: ", order_products_prior[order_products_prior.order_id.isin(orders.order_id)].order_id.nunique())
 
 """2.3. Validate orders matching in the train table"""
-
-# 验证: order_products_train.csv 里面的 order_id 都在 orders.csv 里 (确保所有train orders仍然真实存在)
 print("orders count in train: ", order_products_train['order_id'].nunique())
 print("order_id from train found in orders: ", order_products_train[order_products_train.order_id.isin(orders.order_id)].order_id.nunique())
 
 """2.4. Validate the intersection between prior and train table"""
 
-# 验证：order_products_prior.csv 和 order_products_train.csv 里面的order_id没有交集
+# train and prior are different 
 print("order_id intersection between prior and train: ", pd.merge(order_products_prior, order_products_train, on = ['order_id']).shape[0])
 
 """2.5. Validate the user_id matching in prior and train set"""
 
-# orders.csv里面，不同eval_set中user个数：
+# number of users in each eval set
 orders.groupby(['eval_set'], as_index=False).agg(OrderedDict([('user_id','nunique')]))
 
 prior_user_ids = set(orders[orders['eval_set'] == 'prior']['user_id'])
@@ -166,17 +158,15 @@ print("user_ids in prior: ", len(prior_user_ids))
 print("user_ids in train: ", len(train_user_ids))
 print("intersection of prior and train: ", len(prior_user_ids.intersection(train_user_ids)))
 
-"""2.6. Validate order counts in the train dataset"""
+# 2.6. Validate order counts in the train dataset
 
 (orders[orders.user_id.isin(train_user_ids)][orders.eval_set == 'train']
   .groupby(['user_id'], as_index=False)
   .agg(OrderedDict([('order_number','count')]))
   .rename(columns={'order_number':'order_counts'})).sort_values(by=['order_counts']).head()
 
-"""由此可见，在train dataset里，一个用户只有一个order
+# 2.7. Validate the relative order of `order_num` in prior and train dataset
 
-2.7. Validate the relative order of `order_num` in prior and train dataset
-"""
 
 df_prior_order_max = (orders[orders.user_id.isin(prior_user_ids)][orders.eval_set == 'prior']
   .groupby(['user_id'], as_index=False)
